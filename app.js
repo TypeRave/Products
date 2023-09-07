f2c966f0cea773319b1804e364b7661452a52cf4
Frank Soto <fasfranksoto@gmail.com>
2023-09-07 12:24:34 -0700

    includes loaderio files

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const path = require("path");
 const PORT =  process.env.PORT;
 const app = express();
 const routes = require("./server/routes.js");
+const fs = require('fs');
+
+const fp = path.join(__dirname, './stress_tests/loaderio-a546c5dbb70fe40696825d613ca0516b.txt')
+
 
 app.use(express.json());
-
+app.use('/loaderio-a546c5dbb70fe40696825d613ca0516b', (req, res) =>{
+  // console.log('attempting to open');
+  let body = fs.readFileSync(fp);
+  // console.log('file opened!');
+  res.send(body);
+})
 app.use('/data', routes);
 
 
