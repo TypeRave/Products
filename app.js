d2f8b795033aa2204ab8f703b63112a8a5f35a24
Frank Soto <fasfranksoto@gmail.com>
2023-09-09 15:26:20 -0700

    replaced loader verification

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const routes = require("./server/routes.js");
 const fs = require('fs');
 
 const app = express();
-const compression = require('compression');
 
-const fp = path.join(__dirname, './stress_tests/loaderio-a546c5dbb70fe40696825d613ca0516b.txt')
+
+const fp = path.join(__dirname, './stress_tests/loaderio-c1f22bca593b80cc4d0edea40117f151.txt')
 
 
 app.use(express.json());
-app.use(compression())
-app.use('/loaderio-a546c5dbb70fe40696825d613ca0516b', (req, res) =>{
+
+app.use('/loaderio-c1f22bca593b80cc4d0edea40117f151', (req, res) =>{
 
   let body = fs.readFileSync(fp);
   res.send(body);
@@ -23,6 +23,7 @@ app.use('/', routes);
 
 
 
-
+app.listen(3001);
+console.log('listening at port 3001')
 app.listen(PORT);
 console.log(`Listening at http://localhost:${PORT}`);
